a32c181d16f826eb9aec07f6126cc5bb1586b384
Daniel Mazurok <dimazurok@cps.edu>
2025-10-30 11:14:55 -0500

    finished write up and tic tac toe

--- a4.py
+++ a4.py
@@ -9,9 +9,52 @@ class TTTBoard:
         board - a list of '*'s, 'X's & 'O's. 'X's represent moves by player 'X', 'O's
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
+    def __init__(self):
+        """Initialize a 3x3 tic tac toe board with 9 '*'"""
+        self.board = ['*'] * 9
 
-    pass
+    def __str__(self):
+        """return a string representation of the board """
+        return f"{self.board[0]} {self.board[1]} {self.board[2]}\n{self.board[3]} {self.board[4]} {self.board[5]}\n{self.board[6]} {self.board[7]} {self.board[8]}"
 
+    def make_move(self, player, pos):
+        """Places a move for the player at position pos if valid
+        
+        Args:
+            player - string "X" or "O"
+            pos - integer 0-8 representing board position
+
+        Returns:
+            True if a move was made, False otherwise
+        """
+        if 0 <= pos <= 8 and self.board[pos] == '*':
+            self.board[pos] = player
+            return True
+        return False
+    
+    def has_won(self, player):
+        """Check if the player has won
+        
+        Args:
+            player - string of either "X" or "O"
+
+        Return:
+            True if the player has won, False otherwise
+        """
+        winning_combos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+        for combo in winning_combos:
+            if self.board[combo[0]] == player and self.board[combo[1]] == player and self.board[combo[2]] == player:
+                return True
+        return False
+
+    def game_over(self):
+        """Check if the game is over (someone has won or the board is full)"""
+        return self.has_won("X") or self.has_won("O") or '*' not in self.board
+    
+    def clear(self):
+        """Clear the board for a new game"""
+        self.board = ['*'] * 9
 
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
@@ -61,9 +104,12 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd)
+    print()
     brd.make_move("X", 8)
     brd.make_move("O", 7)
-
+    print(brd)
+    
     assert brd.game_over() == False
 
     brd.make_move("X", 5)
@@ -89,4 +135,4 @@ if __name__ == "__main__":
     print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()
--- a4_writeup.md
+++ a4_writeup.md
@@ -5,7 +5,8 @@ In assignment 4 we created a basic tic tac toe game so that we could learn objec
 ## Reflection Questions
 
 1. What was the most difficult part to tic-tac-toe?
-
+    The most difficult part of creatint tic-tac-toe was keeping track of the game logic. I had to check sure that all the winning combinations and  drawing combinations were included. Testing these was tricky because tic-tac-toe might seem like a small game but there are a lot of different combinations the board could end.
 2. Explain how you would add a computer player to the game.
-
+    In order to add a computer player to the game, I would modify the play tic-tac-toe function so that one of turns is automatically handled by the program, instead of user input. The computer could be programed to make a random valid move or we can make it more advanced by programing the computer to make a strategic move.   
 3. If you add a computer player, explain (doesn't have to be super technical) how you might get the computer player to play the best move every time. *Note - I am not grading this for a correct answer, I just want to know your thoughts on how you might accomplish it.
+    In order to make the computer player make the best move possible, I would use a strategy or algorithim for the computer to make the best move. The computer could look ahead at all the possible moves and see what would work and what wouldn't. Then it could pick a move that leads it to a win, or blocking the user from winning. And if there isn't  any win or block avaiable then it would pick the move that is good for the next round.
